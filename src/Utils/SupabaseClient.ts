49e1cf3797300cf120700021abe55a3aa44d2c9b
yudha-Dlesmana <yudha.dlesmana@gmail.com>
2025-07-10 19:14:35 +0700

    fix: test rolekey supabase

--- src/Utils/SupabaseClient.ts
+++ src/Utils/SupabaseClient.ts
@@ -3,5 +3,5 @@ import { Database } from "../database/fieldTypes";
 
 export const supabase = createClient<Database>(
   process.env.SUPABASE_SUPABASE_URL as string,
-  process.env.SUPABASE_SUPABASE_ANON_KEY as string
+  process.env.SUPABASE_SUPABASE_SERVICE_ROLE_KEY as string
 );
