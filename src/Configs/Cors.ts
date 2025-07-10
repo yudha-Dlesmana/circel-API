c54afc951b6195d943c3e4eea8fca8fa053580b0
yudha-Dlesmana <yudha.dlesmana@gmail.com>
2025-07-10 10:59:04 +0700

    fix: cors

--- src/Configs/Cors.ts
+++ src/Configs/Cors.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 const corsMiddleware = cors({
-  origin: "http://localhost:5173, https://circel-ui.vercel.app/",
+  origin: ["http://localhost:5173", "https://circel-ui.vercel.app/"],
   credentials: true,
 });
 export default corsMiddleware;
